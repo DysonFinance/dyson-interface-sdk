ff51deb57d05dc46b4043b910190fa5d43a779dd
paul-pelith <paul.li@pelith.com>
2023-10-25 15:23:29 +0800

    fix: naming issue

--- src/reads/getPairsNonces.ts
+++ src/reads/getPairsNonces.ts
@@ -3,9 +3,9 @@ import { Address, getAbiItem } from 'viem'
 import DYSON_PAIR_ABI from '@/constants/abis/DysonSwapPair'
 import { prepareFunctionParams } from '@/utils/viem'
 
-export function getPairsNonces(pairAddress: Address) {
+export function getPairsNonces(owner: Address) {
   return prepareFunctionParams({
     abi: getAbiItem({ abi: DYSON_PAIR_ABI, name: 'nonces' }),
-    args: [pairAddress],
+    args: [owner],
   })
 }
