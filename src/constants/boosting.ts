234b12859ca15b9673a46615658960aec632b04a
paul-pelith <paul.li@pelith.com>
2023-11-08 12:15:49 +0800

    fix: MAX_BONIUS type to bigint

--- src/constants/boosting.ts
+++ src/constants/boosting.ts
@@ -1,2 +1,4 @@
-export const MAX_BONUS = 15 * 10 ** 17
+import { parseUnits } from 'viem'
+
+export const MAX_BONUS = parseUnits('1.5', 18)
 export const BOOSTING_DYSN_MULTIPLIER = 225n * 10n ** 35n
